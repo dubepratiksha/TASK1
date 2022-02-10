463b719fadb46fc8f71efd02d97c7c961cb251c7
pratiksha <dubepratiksha18@gmail.com>
2022-02-10 20:41:51 +0530

    initial commit

---
+++ problemsolving1.py
@@ -0,0 +1,11 @@
+str1="I am  Dube Pratiksha \n Currently I am pursuing computer engineering in Jaihind College of Engineering."
+print(str1) #Newline
+
+str2 = "jaihind\tcollage"
+print(str2) #Space/tab
+
+str3 ='computer engineering'
+print(str3)  #Single Quote
+
+str4="python language"
+print(str4) #double Quote
